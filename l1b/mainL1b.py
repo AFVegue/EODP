0dcbb673d85721ba31ff2528cceb7a78c284a70c
AFVegue <alejandrofvgctic@gmail.com>
2026-09-10 19:47:14 +0200

    lib1

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\Users\aleja\OneDrive\Escritorio\EODP\EODP_code\auxiliary'
-indir = r"C:\Users\aleja\OneDrive\Escritorio\EODP\EODP_TER_2021\EODP_TER_2021\EODP-TS-L1B\input"
-outdir = r"C:\Users\aleja\OneDrive\Escritorio\EODP\EODP_TER_2021\EODP_TER_2021\EODP-TS-L1B\output_l1b"
+auxdir = r'C:\\Users\\aleja\\OneDrive\\Escritorio\\EODP\\EODP_code\\auxiliary'
+indir = r"C:\\Users\\aleja\\OneDrive\\Escritorio\\EODP\\EODP_TER_2021\\EODP_TER_2021\\EODP-TS-L1B\\input"
+outdir = r"C:\\Users\\aleja\\OneDrive\\Escritorio\\EODP\\EODP_TER_2021\\EODP_TER_2021\\EODP-TS-L1B\\output_l1b"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
